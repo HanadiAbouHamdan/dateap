3de4e200f90acafef534fb147b7a70203148e72d
HanadiAbouHamdan <hanadi.abouhamdan97@hotmail.com>
2023-03-20 17:24:14 +0200

    Fixing the ProfilepageController so that the picture and the data will be sent to their specific tables

--- app/Http/Controllers/ProfilepageController.php
+++ app/Http/Controllers/ProfilepageController.php
@@ -10,36 +10,36 @@ class ProfilepageController extends Controller
 {
     public function update(Request $request)
     {
-        // Authenticate the user and get their ID
         $user = Auth::user();
-        $user_id = $user->id;
-
-        // Retrieve the profile data from the request
-        $location = $request->input('location');
-        $age = $request->input('age');
-        $gender = $request->input('gender');
-        $bio = $request->input('bio');
-        $interests = $request->input('interests');
-
-        // Update the profile in the database
-        $profiles=Profile::where('user_id',$user_id)->first();
         
-            $profiles->update([
-                'location' => $location,
-                'age' => $age,
-                'gender' => $gender,
-                'bio' => $bio,
-                'interests' => $interests,
+        // Save the uploaded picture to the pictures table and get the URL
+        $pictureUrl = '';
+        if ($request->hasFile('picture')) {
+            $picture = $request->file('picture');
+            $filename = time() . '.' . $picture->getClientOriginalExtension();
+            $picture->storeAs('public/pictures', $filename);
+            $pictureUrl = '/storage/pictures/' . $filename;
+            $picture = Picture::create([
+                'user_id' => $user->id,
+                'url' => $pictureUrl
             ]);
-
-        // Store the profile picture in the database
-        $picture = $request->file('picture')->store('pictures');
-        DB::table('pictures')
-            ->where('user_id', $user_id)
-            ->update([
-                'picture' => $picture,
-            ]);
-
-        return response()->json(['success' => true]);
+        }
+        
+        // Save the rest of the profile information to the profiles table
+        $profile = Profile::updateOrCreate(
+            ['user_id' => $user->id],
+            [
+                'location' => $request->input('location'),
+                'age' => $request->input('age'),
+                'gender' => $request->input('gender'),
+                'bio' => $request->input('bio'),
+                'interests' => $request->input('interests')
+            ]
+        );
+    
+        return response()->json([
+            'message' => 'Profile updated successfully',
+            'picture_url' => $pictureUrl
+        ]);
     }
-}
+}    
